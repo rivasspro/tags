763c2d8fdd744ce60a38bfbacff252a49385f5d3
David Rivas @c3ntro <david.rivas@c3ntro.com>
2023-01-30 19:34:49 -0600

    tags options API

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import InputTag from './components/InputTag.vue';
+export default {
+  components: {
+    InputTag
+  }
+}
+</script>
+<template>
+  <InputTag />
+</template>
+<style>
+
+</style>
---
+++ src/components/InputTag.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  
+  data() {
+    return {
+      currentValue: '',
+      tags: []
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="inputTag">
+    <div class="tags">
+      <div class="tag" v-for="(tag, index) in tags" :key="index">
+      {{ tag }}
+      </div>
+    </div>
+    <input type="text" v-model="currentValue">
+  </div>
+</template>
+
+<style scoped>
+.lo {
+  display: block;
+}
+</style>
